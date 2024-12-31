097643a90460aa8596c7c7dbaab14ab1e14e9e47
vizicsaba <64264159+vizicsaba@users.noreply.github.com>
2024-12-31 17:28:39 +0100

    Update hundredtest.c

--- hundredtest.c
+++ hundredtest.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 
-// Összeg kiszámítása
 int calculate_sum(int n) {
     int sum = 0;
     for (int i = 1; i <= n; i++) {
         sum += i;
     }
     return sum;
-    printf("Az osszeg: %d\\n", sum);
-    
 }
 
-// Átlag kiszámítása
 double calculate_average(int sum, int count) {
     return (double)sum / count;
-    printf("A szamtani atlag: %.2f\\n", calculate_average);
 }
 
-// Csak akkor forduljon, ha NEM tesztelési környezetben használjuk
 #ifndef TESTING
 int main() {
     int count = 100;
     int sum = calculate_sum(count);
     double average = calculate_average(sum, count);
 
-    printf("Az osszeg: %d\\n", sum);
-    printf("A szamtani atlag: %.2f\\n", average);
+    printf("Az osszeg: %d\n", sum);
+    printf("A szamtani atlag: %.2f\n", average);
 
     return 0;
+}
+#else
+void print_results() {
+    int count = 100;
+    int sum = calculate_sum(count);
+    double average = calculate_average(sum, count);
+
+    printf("Az osszeg: %d\n", sum);
+    printf("A szamtani atlag: %.2f\n", average);
 }
 #endif

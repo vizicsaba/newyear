a7881031885382cd57184b5b353e9a93eb2b33c0
vizicsaba <64264159+vizicsaba@users.noreply.github.com>
2024-12-31 16:47:52 +0100

    Update hundredtest.c
    1-től százig összead, számtani átlagot számol

--- hundredtest.c
+++ hundredtest.c
@@ -1,1 +1,20 @@
-#oe-christmas
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    int count = 100;
+
+    // Számok összeadása 1-től 100-ig
+    for (int i = 1; i <= count; i++) {
+        sum += i;
+    }
+
+    // Számtani átlag kiszámítása
+    double average = (double)sum / count;
+
+    // Eredmény kiírása
+    printf("Az osszeg: %d\n", sum);
+    printf("A szamtani atlag: %.2f\n", average);
+
+    return 0;
+}

57aed256df7da3c300be19a5173fa4f77762214d
vizicsaba <64264159+vizicsaba@users.noreply.github.com>
2025-01-01 11:07:11 +0100

    Update test_hundredtest.c

--- test_hundredtest.c
+++ test_hundredtest.c
@@ -18,6 +18,7 @@ void run_tests() {
     // Teszt az átlag kiszámítására
     double average = calculate_average(sum, 100);
     if (average == 50.5) {
+        printf("Avarage result:%.2f\\n", avarage);
         printf("Average test passed.\\n");
     } else {
         printf("Average test failed: expected 50.5, got %.2f\\n", average);
